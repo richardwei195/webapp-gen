7803c331f829b72ab47e36043941e4503021699a
蒋巍 <649581473@qq.com>
2017-12-14 13:40:02 +0800

    fix: fix index.js

--- index.js
+++ index.js
@@ -4,6 +4,36 @@ const program = require('commander')
 const fs = require('fs')
 const path = require('path')
 const readline = require('readline')
+const pkg = require('./package.json')
+
+var _exit = process.exit;
+var version = pkg.version;
+
+// Re-assign process.exit because of commander
+// TODO: Switch to a different command framework
+process.exit = exit
+
+// CLI
+
+around(program, 'optionMissingArgument', function (fn, args) {
+  program.outputHelp()
+  fn.apply(this, args)
+  return { args: [], unknown: [] }
+})
+
+before(program, 'outputHelp', function () {
+  // track if help was shown for unknown option
+  this._helpShown = true
+})
+
+before(program, 'unknownOption', function () {
+  // allow unknown options if help was shown, to prevent trailing error
+  this._allowUnknownOption = this._helpShown
+  // show help if not yet shown
+  if (!this._helpShown) {
+    program.outputHelp()
+  }
+})
 
 class Generator {
   constructor () {
@@ -50,8 +80,60 @@ program
   .option('-j, --javascript', gen.genType = 'javascript')
   .parse(process.argv)
 
-if (!process.exit) {
-  console.log('123', 123)
+if (!exit.exited) {
+  gen.build()
 }
 
-gen.build()
+
+/**
+ * Install an around function; AOP.
+ */
+
+function around(obj, method, fn) {
+  var old = obj[method]
+
+  obj[method] = function () {
+    var args = new Array(arguments.length)
+    for (var i = 0; i < args.length; i++) args[i] = arguments[i]
+    return fn.call(this, old, args)
+  }
+}
+
+/**
+ * Install a before function; AOP.
+ */
+
+function before(obj, method, fn) {
+  var old = obj[method]
+
+  obj[method] = function () {
+    fn.call(this)
+    old.apply(this, arguments)
+  }
+}
+
+/**
+ * Graceful exit for async STDIO
+ */
+
+function exit(code) {
+  // flush output for Node.js Windows pipe bug
+  // https://github.com/joyent/node/issues/6247 is just one bug example
+  // https://github.com/visionmedia/mocha/issues/333 has a good discussion
+  function done() {
+    if (!(draining--)) _exit(code)
+  }
+
+  var draining = 0
+  var streams = [process.stdout, process.stderr]
+
+  exit.exited = true
+
+  streams.forEach(function (stream) {
+    // submit empty write request and wait for completion
+    draining += 1
+    stream.write('', done)
+  })
+
+  done()
+}
